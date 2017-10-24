c62b36c7d1380cd4529db710d8320a455727a82e
denimars <marswandi.denilinux@gmail.com>
2017-10-24 22:00:29 +0800

    go rest

---
+++ database/connect.go
@@ -0,0 +1,21 @@
+package database
+
+import(
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+)
+
+var DB *gorm.DB
+
+func init(){
+	var err error
+	DB, err = gorm.Open("mysql","root:root@/dev?charset=utf8&parseTime=True&loc=Local")
+	if err != nil{
+		log.Println(err.Error)
+	}else{
+		log.Println("Connected... horraaaay")
+	}
+	DB.SingularTable(true)
+	//DB.AutoMigrate()
+}
