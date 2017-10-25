43f60ca3708aee8874331b0dfb2c36eadd7ab40f
denimars <marswandi.denilinux@gmail.com>
2017-10-25 10:41:03 +0800

    add new

--- database/connect.go
+++ database/connect.go
@@ -12,7 +12,7 @@ func init(){
 	var err error
 	DB, err = gorm.Open("mysql","root:root@/dev?charset=utf8&parseTime=True&loc=Local")
 	if err != nil{
-		log.Println(err.Error)
+		log.Println(err)
 	}else{
 		log.Println("Connected... horraaaay")
 	}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
+import "restGoEcho/database"
 
 func main(){
+	database.DB.Close()
 	fmt.Println("test")
 }
